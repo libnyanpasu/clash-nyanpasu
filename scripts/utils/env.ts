9dfbc072f17415b58f32a1ab38007a131da84776
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-12-11 18:48:55 +0800

    feat(manifest): latest version generator

--- scripts/utils/env.ts
+++ scripts/utils/env.ts
@@ -2,4 +2,5 @@ import path from "node:path";
 
 export const cwd = process.cwd();
 export const TAURI_APP_DIR = path.join(cwd, "backend/tauri");
+export const MANIFEST_DIR = path.join(cwd, "manifest");
 export const GITHUB_PROXY = "https://mirror.ghproxy.com/";

4298483c670e4c6d4b8eefe49d38d479e6417f9c
keiko233 <keiko23333@gmail.com>
2023-12-30 20:49:45 +0800

    chore: fix script consola columns

--- scripts/utils/logger.ts
+++ scripts/utils/logger.ts
@@ -6,7 +6,6 @@ export const consola = createConsola({
   level: process.env.LOG_LEVEL ? Number.parseInt(process.env.LOG_LEVEL) : 5,
   fancy: true,
   formatOptions: {
-    columns: 80,
     colors: true,
     compact: false,
     date: true,

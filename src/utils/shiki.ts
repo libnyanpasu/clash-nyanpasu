3de02c58f1e0443ddd9aea22c9a79f458b61362f
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-01-15 19:44:18 +0800

    fix(vite): rm useless shikiji langs support

--- src/utils/shiki.ts
+++ src/utils/shiki.ts
@@ -1,12 +1,19 @@
-import { getHighlighter, type Highlighter } from "shikiji";
+import type { Highlighter } from "shikiji";
+import { getHighlighterCore } from "shikiji/core";
+
+import minLight from "shikiji/themes/min-light.mjs";
+import nord from "shikiji/themes/nord.mjs";
+import getWasm from "shikiji/wasm";
 
 let shiki: Highlighter | null = null;
 
 export async function getShikiSingleton() {
   if (!shiki) {
-    shiki = await getHighlighter({
-      themes: ["nord", "min-light"],
-      langs: ["ansi"],
+    shiki = await getHighlighterCore({
+      themes: [nord, minLight],
+      langs: [],
+
+      loadWasm: getWasm,
     });
   }
   return shiki;

4052f80e57c01e34e62fb4966ed8699c61622725
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-01-27 20:17:48 +0800

    fix(portable): do not use system notification api while app is portable

--- backend/tauri/src/cmds.rs
+++ backend/tauri/src/cmds.rs
@@ -24,6 +24,12 @@ pub fn get_profiles() -> CmdResult<IProfiles> {
     Ok(Config::profiles().data().clone())
 }
 
+#[cfg(target_os = "windows")]
+#[tauri::command]
+pub fn is_portable() -> CmdResult<bool> {
+    Ok(crate::utils::dirs::get_portable_flag())
+}
+
 #[tauri::command]
 pub async fn enhance_profiles() -> CmdResult {
     wrap_err!(CoreManager::global().update_config().await)?;

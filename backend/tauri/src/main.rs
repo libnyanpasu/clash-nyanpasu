909ca96b47cf98d91ed539216a477a3c36eae268
4o3F <4o3f@proton.me>
2024-02-13 21:35:04 +0800

    refactor(i18n): move locales to top level dir

--- backend/tauri/src/main.rs
+++ backend/tauri/src/main.rs
@@ -14,7 +14,7 @@ use crate::config::Config;
 use crate::utils::{init, resolve, server};
 use tauri::{api, SystemTray};
 
-rust_i18n::i18n!("locales");
+rust_i18n::i18n!("../../locales");
 
 fn main() -> std::io::Result<()> {
     // 单例检测

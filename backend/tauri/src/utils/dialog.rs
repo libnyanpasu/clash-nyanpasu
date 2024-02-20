f0aa74d9fd3cd2046cbe045553903e28c5f4e485
Jonson Petard <i@a632079.me>
2024-02-20 19:22:11 +0800

    fix(macos): use rfd to prevent panic

--- backend/tauri/src/utils/dialog.rs
+++ backend/tauri/src/utils/dialog.rs
@@ -1,48 +1,34 @@
+use rfd::{MessageButtons, MessageDialog, MessageDialogResult, MessageLevel};
 use rust_i18n::t;
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc, Barrier,
-};
-use tauri::api::dialog::{MessageDialogBuilder, MessageDialogButtons, MessageDialogKind};
 
 pub fn panic_dialog(msg: &str) {
     let msg = format!("{}\n\n{}", msg, t!("dialog.panic"));
-    let barrier = Arc::new(Barrier::new(2));
-    let barrier_ref = barrier.clone();
-    MessageDialogBuilder::new("Error", msg)
-        .kind(MessageDialogKind::Error)
-        .buttons(MessageDialogButtons::Ok)
-        .show(move |_| {
-            barrier_ref.wait();
-        });
-    barrier.wait();
+    MessageDialog::new()
+        .set_level(MessageLevel::Error)
+        .set_title("Clash Nyanpasu Crash".to_string())
+        .set_description(msg)
+        .set_buttons(MessageButtons::Ok)
+        .show();
 }
 
 pub fn migrate_dialog() -> bool {
     let msg = format!("{}", t!("dialog.migrate"));
-    let barrier = Arc::new(Barrier::new(2));
-    let barrier_ref = barrier.clone();
-    let migrate = Arc::new(AtomicBool::new(false));
-    let migrate_ref = migrate.clone();
-    MessageDialogBuilder::new("Migration", msg)
-        .kind(MessageDialogKind::Warning)
-        .buttons(MessageDialogButtons::YesNo)
-        .show(move |_migrate| {
-            migrate_ref.store(_migrate, Ordering::Relaxed);
-            barrier_ref.wait();
-        });
-    barrier.wait();
-    migrate.load(Ordering::Relaxed)
+    matches!(
+        MessageDialog::new()
+            .set_level(MessageLevel::Warning)
+            .set_title("Clash Nyanpasu Migration".to_string())
+            .set_buttons(MessageButtons::YesNo)
+            .set_description(msg)
+            .show(),
+        MessageDialogResult::Yes
+    )
 }
 
 pub fn error_dialog(msg: String) {
-    let barrier = Arc::new(Barrier::new(2));
-    let barrier_ref = barrier.clone();
-    MessageDialogBuilder::new("Error", msg)
-        .kind(MessageDialogKind::Error)
-        .buttons(MessageDialogButtons::Ok)
-        .show(move |_| {
-            barrier_ref.wait();
-        });
-    barrier.wait();
+    MessageDialog::new()
+        .set_level(MessageLevel::Error)
+        .set_title("Clash Nyanpasu Error".to_string())
+        .set_description(msg)
+        .set_buttons(MessageButtons::Ok)
+        .show();
 }

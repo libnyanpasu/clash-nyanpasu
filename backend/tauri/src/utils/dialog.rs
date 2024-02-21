d68b0be688ebc9ffb8afc4e996f90c4157840e23
greenhat616 <i@a632079.me>
2024-02-21 12:14:16 +0000

    fix: build issue

--- backend/tauri/src/utils/dialog.rs
+++ backend/tauri/src/utils/dialog.rs
@@ -1,34 +1,32 @@
-use rfd::{MessageButtons, MessageDialog, MessageDialogResult, MessageLevel};
+use rfd::{MessageButtons, MessageDialog, MessageLevel};
 use rust_i18n::t;
 
 pub fn panic_dialog(msg: &str) {
     let msg = format!("{}\n\n{}", msg, t!("dialog.panic"));
     MessageDialog::new()
         .set_level(MessageLevel::Error)
-        .set_title("Clash Nyanpasu Crash".to_string())
-        .set_description(msg)
+        .set_title("Clash Nyanpasu Crash")
+        .set_description(msg.as_str())
         .set_buttons(MessageButtons::Ok)
         .show();
 }
 
 pub fn migrate_dialog() -> bool {
     let msg = format!("{}", t!("dialog.migrate"));
-    matches!(
-        MessageDialog::new()
-            .set_level(MessageLevel::Warning)
-            .set_title("Clash Nyanpasu Migration".to_string())
-            .set_buttons(MessageButtons::YesNo)
-            .set_description(msg)
-            .show(),
-        MessageDialogResult::Yes
-    )
+
+    MessageDialog::new()
+        .set_level(MessageLevel::Warning)
+        .set_title("Clash Nyanpasu Migration")
+        .set_buttons(MessageButtons::YesNo)
+        .set_description(msg.as_str())
+        .show()
 }
 
 pub fn error_dialog(msg: String) {
     MessageDialog::new()
         .set_level(MessageLevel::Error)
-        .set_title("Clash Nyanpasu Error".to_string())
-        .set_description(msg)
+        .set_title("Clash Nyanpasu Error")
+        .set_description(msg.as_str())
         .set_buttons(MessageButtons::Ok)
         .show();
 }

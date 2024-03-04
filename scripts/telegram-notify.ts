89d5bd77e1bd4d506c1c986ef4df5800b21b5932
keiko233 <i@elaina.moe>
2024-03-04 13:52:28 +0800

    chore: fix version escape & version get err

--- scripts/telegram-notify.ts
+++ scripts/telegram-notify.ts
@@ -1,8 +1,9 @@
 import { Telegraf } from "telegraf";
 import pkgJson from "../package.json";
 import { array2text } from "./utils";
+import { context, getOctokit } from "@actions/github";
 
-export const sendReleaseNotify = () => {
+export const sendReleaseNotify = async () => {
   if (!process.env.TELEGRAM_TOKEN) {
     throw new Error("TELEGRAM_TOKEN is required");
   }
@@ -11,16 +12,28 @@ export const sendReleaseNotify = () => {
     throw new Error("TELEGRAM_TO is required");
   }
 
+  if (!process.env.GITHUB_TOKEN) {
+    throw new Error("GITHUB_TOKEN is required");
+  }
+
   const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
   const { version } = pkgJson;
 
+  const stringEscape = (str: string) => {
+    return str.replace(/\./g, "\\.").replace(/-/g, "\\-").replace(/_/g, "\\_");
+  };
+
+  const github = getOctokit(process.env.GITHUB_TOKEN);
+
+  const { data: tags } = await github.rest.repos.listTags(context.repo);
+
   bot.telegram.sendMessage(
     process.env.TELEGRAM_TO,
     array2text([
-      `Clash Nyanpasu ${version} Released!`,
+      `Clash Nyanpasu ${stringEscape(version)} Released!`,
       "",
-      `[Check out updates on GitHub](https://github.com/LibNyanpasu/clash-nyanpasu/compare/v${version}...main)`,
+      `[Check out updates on GitHub](https://github.com/LibNyanpasu/clash-nyanpasu/compare/v${tags.at(-2)?.name}...v${version})`,
       "",
       "*Download Link:*",
       ` - https://github.com/LibNyanpasu/clash-nyanpasu/releases/tag/v${version}`,

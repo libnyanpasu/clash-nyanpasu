2591e55e31f5e69b5191cf3930e87bde246bc80d
keiko233 <keiko23333@gmail.com>
2024-04-14 02:33:42 +0800

    chore(eslint): allow namespace

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
     "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-namespace": "off",
   },
   settings: {
     react: {

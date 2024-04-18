9e744e4eb6593c1459f39a26ec3d9288a3a4ae7e
keiko233 <i@elaina.moe>
2024-04-18 16:32:58 +0800

    feat(ui): add MDYBaseCard component

---
+++ frontend/ui/materialYou/components/baseCard/index.tsx
@@ -0,0 +1,28 @@
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import { ReactNode } from "react";
+
+export const BaseCard = ({
+  label,
+  labelChildren,
+  children,
+}: {
+  label: string;
+  labelChildren?: ReactNode;
+  children?: ReactNode;
+}) => {
+  return (
+    <Card>
+      <CardContent>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography sx={{ pb: 1 }} variant="h5" component="div">
+            {label}
+          </Typography>
+
+          {labelChildren}
+        </Box>
+
+        {children}
+      </CardContent>
+    </Card>
+  );
+};

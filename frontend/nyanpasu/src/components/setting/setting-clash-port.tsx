a27564e65ab06f45a54b301a9f90c6b96b77b4e6
keiko233 <i@elaina.moe>
2024-04-19 17:35:58 +0800

    feat(nyanpasu): add SettingClashPort component

---
+++ frontend/nyanpasu/src/components/setting/setting-clash-port.tsx
@@ -0,0 +1,63 @@
+import { List } from "@mui/material";
+import { BaseCard, NumberItem, SwitchItem } from "@nyanpasu/ui";
+import { useTranslation } from "react-i18next";
+import { useClash, useNyanpasu } from "@nyanpasu/interface";
+import { useMemo } from "react";
+import { useMessage } from "@/hooks/use-notification";
+
+export const SettingClashPort = () => {
+  const { t } = useTranslation();
+
+  const { nyanpasuConfig, setNyanpasuConfig } = useNyanpasu();
+
+  const { getConfigs, setConfigs } = useClash();
+
+  const port = useMemo(() => {
+    return (
+      getConfigs.data?.["mixed-port"] ||
+      nyanpasuConfig?.verge_mixed_port ||
+      7890
+    );
+  }, [getConfigs.data?.["mixed-port"], nyanpasuConfig?.verge_mixed_port]);
+
+  return (
+    <BaseCard label={t("Clash Port")}>
+      <List disablePadding>
+        <NumberItem
+          label={t("Mixed Port")}
+          vaule={port}
+          checkEvent={(input) => input > 65535 || input < 1}
+          checkLabel="Port must be between 1 and 65535."
+          onApply={(value) => {
+            setConfigs({ "mixed-port": value });
+            setNyanpasuConfig({ verge_mixed_port: value });
+          }}
+        />
+
+        <SwitchItem
+          label={t("Random Port")}
+          checked={nyanpasuConfig?.enable_random_port || false}
+          onChange={async () => {
+            try {
+              await setNyanpasuConfig({
+                enable_random_port: !nyanpasuConfig?.enable_random_port,
+              });
+            } catch (e) {
+              useMessage(JSON.stringify(e), {
+                title: t("Error"),
+                type: "error",
+              });
+            } finally {
+              useMessage(t("After restart to take effect"), {
+                title: t("Success"),
+                type: "info",
+              });
+            }
+          }}
+        />
+      </List>
+    </BaseCard>
+  );
+};
+
+export default SettingClashPort;

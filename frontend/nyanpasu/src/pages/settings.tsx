790df8b338ce22a2b9b3e515cf7a09d28215f212
keiko233 <keiko23333@gmail.com>
2024-04-28 00:48:20 +0800

    feat(nyanpasu): experimentally added new settings interface

--- frontend/nyanpasu/src/pages/settings.tsx
+++ frontend/nyanpasu/src/pages/settings.tsx
@@ -1,62 +1,81 @@
-import { BasePage } from "@/components/base";
-import SettingClash from "@/components/setting/setting-clash";
-import SettingSystem from "@/components/setting/setting-system";
-import SettingVerge from "@/components/setting/setting-verge";
-import { NotificationType, useNotification } from "@/hooks/use-notification";
-import { openWebUrl } from "@/services/cmds";
 import { GitHub } from "@mui/icons-material";
-import { Grid, IconButton, Paper } from "@mui/material";
+import { IconButton } from "@mui/material";
+import Masonry from "@mui/lab/Masonry";
 import { useLockFn } from "ahooks";
 import { useTranslation } from "react-i18next";
+import { BasePage } from "@nyanpasu/ui";
+import { open } from "@tauri-apps/api/shell";
+import { motion } from "framer-motion";
+import { lazy, Suspense } from "react";
+
+const asyncComponents = [
+  () => import("@/components/setting/setting-system-proxy"),
+  () => import("@/components/setting/setting-nyanpasu-ui"),
+  () => import("@/components/setting/setting-clash-base"),
+  () => import("@/components/setting/setting-clash-port"),
+  () => import("@/components/setting/setting-clash-external"),
+  () => import("@/components/setting/setting-clash-web"),
+  () => import("@/components/setting/setting-clash-field"),
+  () => import("@/components/setting/setting-clash-core"),
+  () => import("@/components/setting/setting-system-behavior"),
+  () => import("@/components/setting/setting-system-service"),
+  () => import("@/components/setting/setting-nyanpasu-tasks"),
+  () => import("@/components/setting/setting-nyanpasu-misc"),
+  () => import("@/components/setting/setting-nyanpasu-path"),
+  () => import("@/components/setting/setting-nyanpasu-version"),
+  () => import("@/components/setting/setting-legacy"),
+];
+
+const GithubIcon = () => {
+  const toGithubRepo = useLockFn(() => {
+    return open("https://github.com/LibNyanpasu/clash-nyanpasu");
+  });
+
+  return (
+    <IconButton
+      color="inherit"
+      title="@keiko233/clash-nyanpasu"
+      onClick={toGithubRepo}
+    >
+      <GitHub fontSize="inherit" />
+    </IconButton>
+  );
+};
 
 export default function SettingPage() {
   const { t } = useTranslation();
 
-  const onError = (err: any) => {
-    useNotification({
-      title: t("Error"),
-      body: err.message || err.toString(),
-      type: NotificationType.Error,
-    });
-  };
-
-  const toGithubRepo = useLockFn(() => {
-    return openWebUrl("https://github.com/LibNyanpasu/clash-nyanpasu");
-  });
-
   return (
-    <BasePage
-      title={t("Settings")}
-      header={
-        <IconButton
-          size="small"
-          color="inherit"
-          title="@keiko233/clash-nyanpasu"
-          onClick={toGithubRepo}
-        >
-          <GitHub fontSize="inherit" />
-        </IconButton>
-      }
-    >
-      <Grid container spacing={{ xs: 2, lg: 3 }}>
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ borderRadius: 1, boxShadow: 2 }}>
-            <SettingClash onError={onError} />
-          </Paper>
-        </Grid>
+    <BasePage title={t("Settings")} header={<GithubIcon />}>
+      <Masonry
+        columns={{ xs: 1, sm: 1, md: 2 }}
+        spacing={3}
+        sx={{ width: "calc(100% + 24px)" }}
+        sequential
+      >
+        {asyncComponents.map((item, index) => {
+          const AsyncComponent = lazy(item);
 
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ borderRadius: 1, boxShadow: 2 }}>
-            <SettingSystem onError={onError} />
-          </Paper>
-        </Grid>
-
-        <Grid item xs={12} md={6}>
-          <Paper sx={{ borderRadius: 1, boxShadow: 2 }}>
-            <SettingVerge onError={onError} />
-          </Paper>
-        </Grid>
-      </Grid>
+          return (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 64 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: index * 0.1 + 0.3,
+                  type: "spring",
+                },
+              }}
+            >
+              <Suspense>
+                <AsyncComponent />
+              </Suspense>
+            </motion.div>
+          );
+        })}
+      </Masonry>
     </BasePage>
   );
 }

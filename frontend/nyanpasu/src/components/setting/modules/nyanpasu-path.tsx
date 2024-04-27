95e5188cf54b425277ed177176717fc73f78c57f
keiko233 <keiko23333@gmail.com>
2024-04-27 21:08:59 +0800

    feat(nyanpasu): add PaperButton component

---
+++ frontend/nyanpasu/src/components/setting/modules/nyanpasu-path.tsx
@@ -0,0 +1,56 @@
+import {
+  alpha,
+  ButtonBase,
+  ButtonBaseProps,
+  Paper,
+  SxProps,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { memo, ReactNode } from "react";
+
+export interface PaperButtonProps extends ButtonBaseProps {
+  label: string;
+  children?: ReactNode;
+  sxPaper?: SxProps;
+  sxButton?: SxProps;
+}
+
+export const PaperButton = memo(function PaperButton({
+  label,
+  children,
+  sxPaper,
+  sxButton,
+  ...props
+}: PaperButtonProps) {
+  const { palette } = useTheme();
+
+  return (
+    <Paper
+      elevation={0}
+      sx={{
+        borderRadius: 6,
+        backgroundColor: alpha(palette.primary.main, 0.1),
+        ...sxPaper,
+      }}
+    >
+      <ButtonBase
+        sx={{
+          borderRadius: 6,
+          width: "100%",
+          textAlign: "start",
+          padding: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          ...sxButton,
+        }}
+        {...props}
+      >
+        <Typography sx={{ fontWeight: 700 }}>{label}</Typography>
+
+        {children}
+      </ButtonBase>
+    </Paper>
+  );
+});
--- frontend/nyanpasu/src/components/setting/modules/system-proxy.tsx
+++ frontend/nyanpasu/src/components/setting/modules/system-proxy.tsx
@@ -1,15 +1,8 @@
-import {
-  ButtonBaseProps,
-  Paper,
-  alpha,
-  ButtonBase,
-  Typography,
-  CircularProgress,
-  useTheme,
-} from "@mui/material";
+import { alpha, CircularProgress, useTheme } from "@mui/material";
 import { memo } from "react";
+import { PaperButton, PaperButtonProps } from "./nyanpasu-path";
 
-export interface PaperSwitchButtonProps extends ButtonBaseProps {
+export interface PaperSwitchButtonProps extends PaperButtonProps {
   label: string;
   checked: boolean;
   loading?: boolean;
@@ -24,10 +17,9 @@ export const PaperSwitchButton = memo(function PaperSwitchButton({
   const { palette } = useTheme();
 
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        borderRadius: 6,
+    <PaperButton
+      label={label}
+      sxPaper={{
         backgroundColor: checked
           ? alpha(palette.primary.main, 0.1)
           : palette.mode == "dark"
@@ -35,23 +27,9 @@ export const PaperSwitchButton = memo(function PaperSwitchButton({
             : palette.common.white,
         cursor: loading ? "progress" : "none",
       }}
+      {...props}
     >
-      <ButtonBase
-        sx={{
-          borderRadius: 6,
-          width: "100%",
-          textAlign: "start",
-          padding: 2,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-        {...props}
-      >
-        <Typography sx={{ fontWeight: 700 }}>{label}</Typography>
-
-        {loading === true && <CircularProgress color="inherit" size={24} />}
-      </ButtonBase>
-    </Paper>
+      {loading === true && <CircularProgress color="inherit" size={24} />}
+    </PaperButton>
   );
 });

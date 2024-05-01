839751f32437587ba08be1d325206abaaa6b9f39
keiko233 <keiko23333@gmail.com>
2024-05-01 13:22:55 +0800

    fix(nyanpasu): missing tailwind css import

--- frontend/nyanpasu/src/main.tsx
+++ frontend/nyanpasu/src/main.tsx
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 import "./assets/styles/index.scss";
+import "./assets/styles/tailwind.css";
 
 import { ResizeObserver } from "@juggle/resize-observer";
 if (!window.ResizeObserver) {

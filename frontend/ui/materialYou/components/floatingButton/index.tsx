d330290ff42befdb704e93441839c8c1fce52d56
keiko233 <keiko23333@gmail.com>
2024-05-19 01:33:58 +0800

    fix(ui): force set FloadtingButton posotion absolute

---
+++ frontend/ui/materialYou/components/floatingButton/index.tsx
@@ -0,0 +1,33 @@
+import { alpha, Button, ButtonProps, useTheme } from "@mui/material";
+import { ReactNode } from "react";
+
+export interface FloatingButtonProps extends ButtonProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export const FloatingButton = ({
+  children,
+  className,
+  ...props
+}: FloatingButtonProps) => {
+  const { palette } = useTheme();
+
+  return (
+    <Button
+      className={`size-16 backdrop-blur !rounded-2xl z-10 bottom-8 right-8 ${className}`}
+      sx={{
+        position: "absolute",
+        boxShadow: 8,
+        backgroundColor: alpha(palette.primary.main, 0.3),
+
+        "&:hover": {
+          backgroundColor: alpha(palette.primary.main, 0.45),
+        },
+      }}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+};

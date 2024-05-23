a54c754421086c185962cc7158aaca38dd0f8592
keiko233 <i@elaina.moe>
2024-05-23 17:10:53 +0800

    chore(script): support send release to telegram channel

--- scripts/telegram-notify.ts
+++ scripts/telegram-notify.ts
@@ -1,47 +1,111 @@
-import { Telegraf } from "telegraf";
-import pkgJson from "../package.json";
-import { array2text } from "./utils";
-import { context, getOctokit } from "@actions/github";
+// import { Telegraf } from "telegraf";
+import { version } from "../package.json";
+import { getOctokit } from "@actions/github";
+import { consola } from "./utils/logger";
+import { client } from "./utils/telegram";
+import { downloadFile } from "./utils/download";
+import path from "path";
+import { TEMP_DIR } from "./utils/env";
+import { GIT_SHORT_HASH } from "./utils/shell";
 
-export const sendReleaseNotify = async () => {
-  if (!process.env.TELEGRAM_TOKEN) {
-    throw new Error("TELEGRAM_TOKEN is required");
-  }
+const nightlyBuild = process.argv.includes("--nightly");
 
-  if (!process.env.TELEGRAM_TO) {
-    throw new Error("TELEGRAM_TO is required");
-  }
+if (!process.env.TELEGRAM_TOKEN) {
+  throw new Error("TELEGRAM_TOKEN is required");
+}
 
-  if (!process.env.GITHUB_TOKEN) {
-    throw new Error("GITHUB_TOKEN is required");
-  }
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 
-  const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+if (!process.env.TELEGRAM_TO) {
+  throw new Error("TELEGRAM_TO is required");
+}
 
-  const { version } = pkgJson;
+const TELEGRAM_TO = process.env.TELEGRAM_TO;
 
-  const stringEscape = (str: string) => {
-    return str.replace(/\./g, "\\.").replace(/-/g, "\\-").replace(/_/g, "\\_");
-  };
+if (!process.env.TELEGRAM_TO_NIGHTLY) {
+  throw new Error("TELEGRAM_TO_NIGHTLY is required");
+}
 
-  const github = getOctokit(process.env.GITHUB_TOKEN);
+const TELEGRAM_TO_NIGHTLY = process.env.TELEGRAM_TO_NIGHTLY;
 
-  const { data: tags } = await github.rest.repos.listTags(context.repo);
+if (!process.env.GITHUB_TOKEN) {
+  throw new Error("GITHUB_TOKEN is required");
+}
 
-  bot.telegram.sendMessage(
-    process.env.TELEGRAM_TO,
-    array2text([
-      `Clash Nyanpasu ${stringEscape(version)} Released\!`,
-      "",
-      `[Check out updates on GitHub](https://github.com/LibNyanpasu/clash-nyanpasu/compare/v${tags[1]?.name}...v${version})`,
-      "",
-      "*Download Link:*",
-      ` - https://github.com/LibNyanpasu/clash-nyanpasu/releases/tag/v${version}`,
-    ]),
-    {
-      parse_mode: "MarkdownV2",
-    },
-  );
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+const resourceFormats = [
+  "x64-setup.exe",
+  "x64_portable.zip",
+  "amd64.AppImage",
+  "amd64.deb",
+  "x64.dmg",
+  "aarch64.dmg",
+];
+
+const isValidFormat = (fileName: string): boolean => {
+  return resourceFormats.some((format) => fileName.endsWith(format));
 };
 
-sendReleaseNotify();
+(async () => {
+  await client.start({
+    botAuthToken: TELEGRAM_TOKEN,
+  });
+
+  const github = getOctokit(GITHUB_TOKEN);
+
+  const content = nightlyBuild
+    ? await github.rest.repos.getReleaseByTag({
+        owner: "LibNyanpasu",
+        repo: "clash-nyanpasu",
+        tag: "pre-release",
+      })
+    : await github.rest.repos.getLatestRelease();
+
+  const downloadTasks: Promise<void>[] = [];
+
+  const reourceMappping: string[] = [];
+
+  content.data.assets.forEach((asset) => {
+    if (isValidFormat(asset.name)) {
+      const _path = path.join(TEMP_DIR, asset.name);
+
+      reourceMappping.push(_path);
+
+      downloadTasks.push(downloadFile(asset.browser_download_url, _path));
+    }
+  });
+
+  await Promise.all(downloadTasks);
+
+  consola.start("Staring upload tasks (nightly)");
+
+  await client.sendFile(TELEGRAM_TO_NIGHTLY, {
+    file: reourceMappping,
+    forceDocument: true,
+    caption: `Clash Nyanpasu Nighly Build ${GIT_SHORT_HASH}`,
+  });
+
+  consola.success("Upload finished (nightly)");
+
+  if (!nightlyBuild) {
+    consola.start("Staring upload tasks (release)");
+
+    await client.sendFile(TELEGRAM_TO, {
+      file: reourceMappping,
+      forceDocument: true,
+      caption: [
+        `Clash Nyanpasu ${version} Released!`,
+        "",
+        "Check out on GitHub:",
+        ` - https://github.com/LibNyanpasu/clash-nyanpasu/releases/tag/v${version}`,
+      ],
+    });
+
+    consola.success("Upload finished (release)");
+  }
+
+  await client.disconnect();
+
+  process.exit();
+})();
---
+++ scripts/utils/shell.ts
@@ -0,0 +1,5 @@
+import { execSync } from "child_process";
+
+export const GIT_SHORT_HASH = execSync("git rev-parse --short HEAD")
+  .toString()
+  .trim();

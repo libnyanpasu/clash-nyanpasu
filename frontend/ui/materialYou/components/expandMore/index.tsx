7af20d7d96a062a71723045f33add94ce87a3630
keiko233 <keiko23333@gmail.com>
2024-05-27 00:28:58 +0800

    feat(ui): add reverse icon props for ExpandMore component

--- frontend/ui/materialYou/components/expandMore/index.tsx
+++ frontend/ui/materialYou/components/expandMore/index.tsx
@@ -4,6 +4,7 @@ import useTheme from "@mui/material/styles/useTheme";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
+  reverse?: boolean;
 }
 
 /**
@@ -15,14 +16,20 @@ interface ExpandMoreProps extends IconButtonProps {
  * @author keiko233 <i@elaina.moe>
  * @copyright LibNyanpasu org. 2024
  */
-export const ExpandMore = ({ expand, ...props }: ExpandMoreProps) => {
+export const ExpandMore = ({ expand, reverse, ...props }: ExpandMoreProps) => {
   const { transitions } = useTheme();
 
   return (
     <IconButton {...props}>
       <ExpandMoreIcon
         sx={{
-          transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+          transform: !expand
+            ? reverse
+              ? "rotate(180deg)"
+              : "rotate(0deg)"
+            : reverse
+              ? "rotate(0deg)"
+              : "rotate(180deg)",
           marginLeft: "auto",
           transition: transitions.create("transform", {
             duration: transitions.duration.shortest,

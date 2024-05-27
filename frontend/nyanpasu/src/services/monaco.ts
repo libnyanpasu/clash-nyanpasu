d9edfeeac8df34177c0de9278728b4f8edada9a0
keiko233 <i@elaina.moe>
2024-05-27 14:23:58 +0800

    feat(monaco): add typescript language service

--- frontend/nyanpasu/src/services/monaco.ts
+++ frontend/nyanpasu/src/services/monaco.ts
@@ -6,6 +6,9 @@ import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution.
 import "monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution.js";
 import "monaco-editor/esm/vs/basic-languages/lua/lua.contribution.js";
 
+// language services
+import "monaco-editor/esm/vs/language/typescript/monaco.contribution.js";
+
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
 export { monaco };

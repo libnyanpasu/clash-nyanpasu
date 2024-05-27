a882a53edc4680d186234458164b79e9d95fdf6c
keiko233 <i@elaina.moe>
2024-05-27 16:26:15 +0800

    feat(experimental): initial react compiler support

--- frontend/nyanpasu/vite.config.ts
+++ frontend/nyanpasu/vite.config.ts
@@ -48,7 +48,17 @@ export default defineConfig(({ command }) => {
       react({
         // jsxImportSource: "@emotion/react",
         babel: {
-          plugins: ["@emotion/babel-plugin"],
+          plugins: [
+            "@emotion/babel-plugin",
+            [
+              "babel-plugin-react-compiler",
+              {
+                sources: (filename) => {
+                  return filename.indexOf("src") !== -1;
+                },
+              },
+            ],
+          ],
         },
       }),
       generouted(),

b8332a1a1d66480901a6416618d283d4fd178f07
keiko233 <i@elaina.moe>
2024-05-27 14:23:12 +0800

    feat(monaco): import lua language support

--- frontend/nyanpasu/src/services/monaco.ts
+++ frontend/nyanpasu/src/services/monaco.ts
@@ -4,6 +4,7 @@ import "monaco-editor/esm/vs/editor/editor.all.js";
 // langs
 import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution.js";
 import "monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution.js";
+import "monaco-editor/esm/vs/basic-languages/lua/lua.contribution.js";
 
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 

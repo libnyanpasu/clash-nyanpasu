e4f09921721c721e673d98e9fdf80fe84bb6d647
keiko233 <keiko23333@gmail.com>
2024-06-17 23:07:05 +0800

    feat(web): replace default utl to Dashboard Page

--- backend/tauri/src/utils/resolve.rs
+++ backend/tauri/src/utils/resolve.rs
@@ -120,7 +120,7 @@ pub fn create_window(app_handle: &AppHandle) {
     let mut builder = tauri::window::WindowBuilder::new(
         app_handle,
         "main".to_string(),
-        tauri::WindowUrl::App("/proxies".into()),
+        tauri::WindowUrl::App("/dashboard".into()),
     )
     .title("Clash Nyanpasu")
     .fullscreen(false)

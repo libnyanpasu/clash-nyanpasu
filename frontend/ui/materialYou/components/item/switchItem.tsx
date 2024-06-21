fde08363400710138a922b2d1d3bd94b2f6f5b6b
keiko233 <i@elaina.moe>
2024-06-21 17:49:43 +0800

    feat(ui): replace Switch to LoadingSwitch for SwitchItem

--- frontend/ui/materialYou/components/item/switchItem.tsx
+++ frontend/ui/materialYou/components/item/switchItem.tsx
@@ -1,14 +1,41 @@
-import { Switch, SwitchProps } from "@mui/material";
+import { SwitchProps } from "@mui/material";
 import { BaseItem } from "./baseItem";
+import { ChangeEvent, useState } from "react";
+import LoadingSwitch from "../loadingSwitch";
 
 interface Props extends SwitchProps {
   label: string;
+  onChange?: (
+    event: ChangeEvent<HTMLInputElement>,
+    checked: boolean,
+  ) => Promise<void> | void;
 }
 
-export const SwitchItem = ({ label, ...switchProps }: Props) => {
+export const SwitchItem = ({ label, onChange, ...switchProps }: Props) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = async (
+    event: ChangeEvent<HTMLInputElement>,
+    checked: boolean,
+  ) => {
+    if (onChange) {
+      try {
+        setLoading(true);
+
+        await onChange(event, checked);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
   return (
     <BaseItem title={label}>
-      <Switch {...switchProps} />
+      <LoadingSwitch
+        loading={loading}
+        onChange={handleChange}
+        {...switchProps}
+      />
     </BaseItem>
   );
 };

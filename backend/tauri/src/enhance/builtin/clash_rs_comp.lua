629e8b0a96ee58dc669149edc8b420f90240ef22
Petard Jonson <41122242+greenhat616@users.noreply.github.com>
2024-08-02 19:34:01 +0800

    fix(enhance): transform allow lan decrepation

--- backend/tauri/src/enhance/builtin/clash_rs_comp.lua
+++ backend/tauri/src/enhance/builtin/clash_rs_comp.lua
@@ -1,5 +1,15 @@
+-- compatible with ipv6 decrepation 
 if config["ipv6"] ~= nil then
     config["ipv6"] = nil
+    if config["dns"] ~= nil and config["dns"]["enabled"] == true then
+        config["dns"]["ipv6"] = true
+    end
+end
+
+-- compatible with allow lan decrepation
+if config["allow_lan"] == true then
+    config["allow_lan"] = nil
+    config["bind_address"] = "0.0.0.0"
 end
 
 return config;

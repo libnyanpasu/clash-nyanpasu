7a8d6f54950fe604e69ef58089558d02a88eb8d9
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-09-10 11:40:15 +0800

    chore: add proxies port type cast for clash-rs
    chore: add proxies port type cast for clash-rs

--- backend/tauri/src/enhance/builtin/clash_rs_comp.lua
+++ backend/tauri/src/enhance/builtin/clash_rs_comp.lua
@@ -12,4 +12,14 @@ if config["allow_lan"] == true then
     config["bind_address"] = "0.0.0.0"
 end
 
+-- compatible with proxies strict port type
+if config["proxies"] ~= nil and type(config["proxies"]) == "table" then
+    for _, proxy in pairs(config["proxies"]) do
+        if proxy["port"] ~= nil and type(proxy["port"]) == "string" then
+            proxy["port"] = tonumber(proxy["port"]) or error("invalid port: " .. proxy["port"])
+        end
+    end
+end
+
+
 return config

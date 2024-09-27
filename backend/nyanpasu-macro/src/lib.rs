fa6f218331ddcb6522ce72ee6ccd76344861a994
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-09-27 12:43:55 +0800

    refactor!: remove fussiness patch! with derive proc macro
    refactor!: remove fussiness patch! with derive proc macro

--- backend/nyanpasu-macro/src/lib.rs
+++ backend/nyanpasu-macro/src/lib.rs
@@ -2,11 +2,21 @@ use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
 mod builder_update;
+mod verge_patch;
 
 #[proc_macro_derive(BuilderUpdate, attributes(update))]
 pub fn builder_update(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     match builder_update::builder_update(input) {
+        Ok(token_stream) => TokenStream::from(token_stream),
+        Err(e) => TokenStream::from(e.to_compile_error()),
+    }
+}
+
+#[proc_macro_derive(VergePatch, attributes(verge))]
+pub fn verge_patch(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    match verge_patch::verge_patch(input) {
         Ok(token_stream) => TokenStream::from(token_stream),
         Err(e) => TokenStream::from(e.to_compile_error()),
     }
---
+++ backend/nyanpasu-macro/src/verge_patch.rs
@@ -0,0 +1,87 @@
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+use syn::{spanned::Spanned, Data, DeriveInput, Error, Ident, LitStr, Meta, Result};
+
+pub fn verge_patch(input: DeriveInput) -> Result<TokenStream> {
+    let name = &input.ident;
+    let mut patch_fn: Option<Ident> = None;
+    let mut patch_pointer: Option<Ident> = None; // default is self
+    let mut patch_type: Option<Ident> = None; // default is Self
+    for attr in &input.attrs {
+        if attr.path().is_ident("verge") {
+            match &attr.meta {
+                Meta::List(ref list) => {
+                    list.parse_nested_meta(|meta| {
+                        match &meta.path {
+                            path if path.is_ident("patch_fn") => {
+                                let value = meta.value()?;
+                                let lit_str: LitStr = value.parse()?;
+                                patch_fn = Some(lit_str.parse()?);
+                            }
+                            path if path.is_ident("patch_pointer") => {
+                                let value = meta.value()?;
+                                let lit_str: LitStr = value.parse()?;
+                                patch_pointer = Some(lit_str.parse()?);
+                            }
+                            path if path.is_ident("patch_type") => {
+                                let value = meta.value()?;
+                                let lit_str: LitStr = value.parse()?;
+                                patch_type = Some(lit_str.parse()?);
+                            }
+                            _ => {
+                                return Err(meta.error("Unknown attribute"));
+                            }
+                        }
+                        Ok(())
+                    })?;
+                }
+                _ => {
+                    return Err(Error::new(attr.span(), "Only #[verge(...)] is supported"));
+                }
+            }
+        }
+    }
+
+    let patch_fn = match patch_fn {
+        Some(fn_name) => fn_name,
+        None => format_ident!("patch_{}", name),
+    };
+    let patch_pointer = match patch_pointer {
+        Some(pointer) => pointer,
+        None => format_ident!("self"),
+    };
+    let patch_type = match patch_type {
+        Some(ty) => ty,
+        None => format_ident!("{}", name),
+    };
+
+    let mut patch_fields = quote! {};
+
+    match input.data {
+        Data::Struct(ref data) => {
+            if let syn::Fields::Named(ref fields) = data.fields {
+                for field in &fields.named {
+                    let field_name = field.ident.as_ref().unwrap();
+                    patch_fields.extend(quote! {
+                        if patch.#field_name.is_some() {
+                            #patch_pointer.#field_name = patch.#field_name;
+                        }
+                    });
+                }
+            }
+        }
+        _ => {
+            return Err(Error::new(input.span(), "Only struct is supported"));
+        }
+    }
+
+    let expanded = quote! {
+        impl #name {
+            pub fn #patch_fn(&mut self, patch: #patch_type) {
+                #patch_fields
+            }
+        }
+    };
+
+    Ok(expanded)
+}

3558d58e13be542687be1751d4498901c9b7b998
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-10-08 16:16:59 +0800

    fix: patch remote profile

--- backend/tauri/src/config/profile/item/remote.rs
+++ backend/tauri/src/config/profile/item/remote.rs
@@ -55,6 +55,7 @@ pub struct RemoteProfile {
         build = "self.option.build().map_err(Into::into)?"
     ))]
     #[builder_update(nested)]
+    #[builder_field_attr(serde(default))]
     pub option: RemoteProfileOptions,
     /// process chains
     #[builder(default)]

f42d2b111bdbabb922f0105aa95b61a5553d2fda
Nyan_yay <74826767+MNDIA@users.noreply.github.com>
2024-10-23 17:04:08 +0800

    prettier

--- backend/tauri/src/enhance/builtin/config_fixer.js
+++ backend/tauri/src/enhance/builtin/config_fixer.js
@@ -1,6 +1,6 @@
 export default function main(params) {
-    if (typeof params['log-level'] === 'boolean') {
-        params['log-level'] = 'debug';
-      }
-    return params;
+  if (typeof params["log-level"] === "boolean") {
+    params["log-level"] = "debug";
+  }
+  return params;
 }

fa237098bcbc94c5aa1bceea3254450d08653562
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2024-12-16 11:55:51 +0800

    chore: migrate to eslint v9

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,31 +1,31 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  endOfLine: "lf",
-  semi: true,
-  singleQuote: false,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
   bracketSpacing: true,
   tabWidth: 2,
-  trailingComma: "all",
+  trailingComma: 'all',
   overrides: [
     {
-      files: ["tsconfig.json", "jsconfig.json"],
+      files: ['tsconfig.json', 'jsconfig.json'],
       options: {
-        parser: "jsonc",
+        parser: 'jsonc',
       },
     },
   ],
   importOrder: [
-    "^@nyanpasu/ui/(.*)$",
-    "^@nyanpasu/interface/(.*)$",
-    "^@/(.*)$",
-    "^@(.*)$",
-    "^[./]",
+    '^@nyanpasu/ui/(.*)$',
+    '^@nyanpasu/interface/(.*)$',
+    '^@/(.*)$',
+    '^@(.*)$',
+    '^[./]',
   ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "5.0.0",
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
   plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-    "prettier-plugin-toml",
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-toml',
   ],
-};
+}

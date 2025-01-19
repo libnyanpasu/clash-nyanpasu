a077a1d76ae140b2b0deee1f568510537c9b561a
3gf8jv4dv <3gf8jv4dv@gmail.com>
2025-01-19 08:34:23 +0800

    feat(i18n): impl zh-tw language
    Special thanks: Fanhuaji <https://zhconvert.org/>
    
    Revised again based on fanhuaji's conversion.

--- frontend/nyanpasu/src/services/i18n.ts
+++ frontend/nyanpasu/src/services/i18n.ts
@@ -2,12 +2,14 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from '@/locales/en.json'
 import ru from '@/locales/ru.json'
+import tw from '@/locales/tw.json'
 import zh from '@/locales/zh.json'
 
 const resources = {
   en: { translation: en },
   ru: { translation: ru },
   zh: { translation: zh },
+  tw: { translation: tw },
 }
 
 i18n.use(initReactI18next).init({

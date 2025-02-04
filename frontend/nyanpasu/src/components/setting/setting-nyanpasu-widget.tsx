c66414d335ec1c05bd197b2dc172598159b617d0
keiko233 <i@elaina.moe>
2025-02-04 14:30:20 +0800

    feat(setting): update network statistic widget to use variant selection

--- frontend/nyanpasu/src/components/setting/setting-nyanpasu-widget.tsx
+++ frontend/nyanpasu/src/components/setting/setting-nyanpasu-widget.tsx
@@ -1,46 +1,46 @@
 import { useTranslation } from 'react-i18next'
 import { List } from '@mui/material'
-import { StatisticWidgetVariant, useSetting } from '@nyanpasu/interface'
-import { BaseCard, Expand, MenuItem, SwitchItem } from '@nyanpasu/ui'
-
-const NetworkWidgetEnable = () => {
-  const { t } = useTranslation()
-
-  const { value, upsert } = useSetting('network_statistic_widget')
-
-  return (
-    <SwitchItem
-      label={t('Enable')}
-      checked={value?.kind === 'enabled'}
-      onChange={async () =>
-        await upsert({
-          kind: value?.kind === 'enabled' ? 'disabled' : 'enabled',
-          value: 'small',
-        })
-      }
-    />
-  )
-}
+import { NetworkStatisticWidgetConfig, useSetting } from '@nyanpasu/interface'
+import { BaseCard, MenuItem } from '@nyanpasu/ui'
 
 const NetworkWidgetVariant = () => {
   const { t } = useTranslation()
 
   const { value, upsert } = useSetting('network_statistic_widget')
 
+  const options = {
+    disabled: t('Disabled'),
+    small: 'Small',
+    large: 'Large',
+  }
+
+  const mapping: { [key: string]: NetworkStatisticWidgetConfig } = {
+    disabled: {
+      kind: 'disabled',
+    },
+    small: {
+      kind: 'enabled',
+      value: 'small',
+    },
+    large: {
+      kind: 'enabled',
+      value: 'large',
+    },
+  }
+
   return (
-    <Expand open={value?.kind === 'enabled'}>
-      <MenuItem
-        label={t('Network Statistic Widget Variant')}
-        options={{
-          small: 'Small',
-          large: 'Large',
-        }}
-        selected={value?.kind === 'enabled' ? value.value : 'small'}
-        onSelected={(val) =>
-          upsert({ kind: 'enabled', value: val as StatisticWidgetVariant })
-        }
-      />
-    </Expand>
+    <MenuItem
+      label={t('Network Statistic Widget Variant')}
+      options={options}
+      selected={
+        Object.entries(mapping).find(([_, config]) =>
+          config.kind === 'disabled'
+            ? value?.kind === 'disabled'
+            : value?.kind === 'enabled' && config.value === value.value,
+        )?.[0] || 'disabled'
+      }
+      onSelected={(val) => upsert(mapping[val as string])}
+    />
   )
 }
 
@@ -50,8 +50,6 @@ export const SettingNyanpasuWidget = () => {
   return (
     <BaseCard label={t('Network Statistic Widget')}>
       <List disablePadding>
-        <NetworkWidgetEnable />
-
         <NetworkWidgetVariant />
       </List>
     </BaseCard>

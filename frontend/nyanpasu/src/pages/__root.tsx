766d4c4153ef769cd52c08db7631f02ebaf2d4e4
keiko233 <i@elaina.moe>
2025-05-04 20:53:36 +0800

    chore(deps): update @tanstack/router-devtools to @tanstack/react-router-devtools

--- frontend/nyanpasu/src/pages/__root.tsx
+++ frontend/nyanpasu/src/pages/__root.tsx
@@ -56,7 +56,7 @@ const TanStackRouterDevtools = import.meta.env.PROD
   ? () => null // Render nothing in production
   : lazy(() =>
       // Lazy load in development
-      import('@tanstack/router-devtools').then((res) => ({
+      import('@tanstack/react-router-devtools').then((res) => ({
         default: res.TanStackRouterDevtools,
         // For Embedded Mode
         // default: res.TanStackRouterDevtoolsPanel

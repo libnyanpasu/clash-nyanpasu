18fa6d7563a881628526974ae1dd1bea82183776
keiko233 <i@elaina.moe>
2025-12-18 21:35:55 +0800

    refactor(chain): simplify chains function by removing useCallback

--- frontend/nyanpasu/src/utils/chain.ts
+++ frontend/nyanpasu/src/utils/chain.ts
@@ -1,18 +1,11 @@
-import { useCallback } from 'react'
-
 export function chains<T>(
   ...handlers: Array<((event: T) => void) | undefined>
 ) {
-  const fn = useCallback(
-    (event: T) => {
-      handlers.forEach((handler) => {
-        if (handler) {
-          handler(event)
-        }
-      })
-    },
-    [handlers],
-  )
-
-  return fn
+  return (event: T) => {
+    handlers.forEach((handler) => {
+      if (handler) {
+        handler(event)
+      }
+    })
+  }
 }

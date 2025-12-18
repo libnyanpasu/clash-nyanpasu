2d778e8bdbfb854a6d76d7fde32aae3ca23f676f
keiko233 <i@elaina.moe>
2025-12-18 21:30:10 +0800

    feat(animated-outlet): add AnimatedOutlet and AnimatedOutletPreset components

---
+++ frontend/nyanpasu/src/components/router/animated-outlet.tsx
@@ -0,0 +1,85 @@
+import { AnimatePresence, motion, useIsPresent } from 'framer-motion'
+import { cloneDeep } from 'lodash-es'
+import { ComponentProps, useContext, useRef } from 'react'
+import {
+  getRouterContext,
+  Outlet,
+  useMatch,
+  useMatches,
+} from '@tanstack/react-router'
+
+export function AnimatedOutlet({
+  ref,
+  ...props
+}: ComponentProps<typeof motion.div>) {
+  const isPresent = useIsPresent()
+
+  const matches = useMatches()
+  const prevMatches = useRef(matches)
+
+  const RouterContext = getRouterContext()
+  const routerContext = useContext(RouterContext)
+
+  let renderedContext = routerContext
+
+  if (isPresent) {
+    prevMatches.current = cloneDeep(matches)
+  } else {
+    renderedContext = cloneDeep(routerContext)
+    renderedContext.__store.state.matches = [
+      ...matches.map((m, i) => ({
+        ...(prevMatches.current[i] || m),
+        id: m.id,
+      })),
+      ...prevMatches.current.slice(matches.length),
+    ]
+  }
+
+  return (
+    <motion.div ref={ref} {...props}>
+      <RouterContext.Provider value={renderedContext}>
+        <Outlet />
+      </RouterContext.Provider>
+    </motion.div>
+  )
+}
+
+export function AnimatedOutletPreset(props: ComponentProps<typeof motion.div>) {
+  const matches = useMatches()
+  const match = useMatch({ strict: false })
+  const nextMatchIndex = matches.findIndex((d) => d.id === match.id) + 1
+  const nextMatch = matches[nextMatchIndex]
+
+  return (
+    <AnimatePresence mode="popLayout" initial={false}>
+      <AnimatedOutlet
+        key={nextMatch.id}
+        layout
+        layoutId={nextMatch.id}
+        initial="initial"
+        animate="visible"
+        exit="hidden"
+        variants={{
+          initial: {
+            translateX: '30%',
+            opacity: 0,
+          },
+          visible: {
+            translateX: '0%',
+            opacity: 1,
+          },
+          hidden: {
+            translateX: '-30%',
+            opacity: 0,
+          },
+        }}
+        transition={{
+          type: 'spring',
+          bounce: 0.1,
+          duration: 0.35,
+        }}
+        {...props}
+      />
+    </AnimatePresence>
+  )
+}
--- frontend/nyanpasu/src/pages/(experimental)/route.tsx
+++ frontend/nyanpasu/src/pages/(experimental)/route.tsx
@@ -1,14 +1,6 @@
-import { AnimatePresence, motion, useIsPresent } from 'framer-motion'
-import { cloneDeep } from 'lodash-es'
-import { ComponentProps, useContext, useRef } from 'react'
+import { AnimatedOutletPreset } from '@/components/router/animated-outlet'
 import { cn } from '@nyanpasu/ui'
-import {
-  createFileRoute,
-  getRouterContext,
-  Outlet,
-  useMatch,
-  useMatches,
-} from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import Header from './_modules/header'
 import Navbar from './_modules/navbar'
 
@@ -16,82 +8,17 @@ export const Route = createFileRoute('/(experimental)')({
   component: RouteComponent,
 })
 
-function AnimatedOutlet({ ref, ...props }: ComponentProps<typeof motion.div>) {
-  const isPresent = useIsPresent()
-
-  const matches = useMatches()
-  const prevMatches = useRef(matches)
-
-  const RouterContext = getRouterContext()
-  const routerContext = useContext(RouterContext)
-
-  let renderedContext = routerContext
-
-  if (isPresent) {
-    prevMatches.current = cloneDeep(matches)
-  } else {
-    renderedContext = cloneDeep(routerContext)
-    renderedContext.__store.state.matches = [
-      ...matches.map((m, i) => ({
-        ...(prevMatches.current[i] || m),
-        id: m.id,
-      })),
-      ...prevMatches.current.slice(matches.length),
-    ]
-  }
-
-  return (
-    <motion.div ref={ref} {...props}>
-      <RouterContext.Provider value={renderedContext}>
-        <Outlet />
-      </RouterContext.Provider>
-    </motion.div>
-  )
-}
-
 const AppContent = () => {
-  const matches = useMatches()
-  const match = useMatch({ strict: false })
-  const nextMatchIndex = matches.findIndex((d) => d.id === match.id) + 1
-  const nextMatch = matches[nextMatchIndex]
-
   return (
-    <AnimatePresence mode="popLayout" initial={false}>
-      <AnimatedOutlet
-        className={cn(
-          'h-[calc(100vh-40px-64px)]',
-          'sm:h-[calc(100vh-40px-48px)]',
-          'overflow-hidden',
-          'bg-white dark:bg-black',
-        )}
-        data-slot="app-content"
-        key={nextMatch.id}
-        layout
-        layoutId={nextMatch.id}
-        initial="initial"
-        animate="visible"
-        exit="hidden"
-        variants={{
-          initial: {
-            translateX: '30%',
-            opacity: 0,
-          },
-          visible: {
-            translateX: '0%',
-            opacity: 1,
-          },
-          hidden: {
-            translateX: '-30%',
-            opacity: 0,
-          },
-        }}
-        transition={{
-          type: 'spring',
-          bounce: 0.1,
-          duration: 0.35,
-        }}
-      />
-    </AnimatePresence>
+    <AnimatedOutletPreset
+      className={cn(
+        'h-[calc(100vh-40px-64px)]',
+        'sm:h-[calc(100vh-40px-48px)]',
+        'overflow-hidden',
+        'bg-white dark:bg-black',
+      )}
+      data-slot="app-content"
+    />
   )
 }
 

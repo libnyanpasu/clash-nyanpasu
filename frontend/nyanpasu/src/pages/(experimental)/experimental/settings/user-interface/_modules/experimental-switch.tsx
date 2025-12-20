e4c2d11f6e32d1eb50af8517f636d921fcb244d7
keiko233 <i@elaina.moe>
2025-12-20 23:34:42 +0800

    fix(experimental-switch): update navigation path to dashboard and set memorized route

--- frontend/nyanpasu/src/pages/(experimental)/experimental/settings/user-interface/_modules/experimental-switch.tsx
+++ frontend/nyanpasu/src/pages/(experimental)/experimental/settings/user-interface/_modules/experimental-switch.tsx
@@ -1,13 +1,19 @@
+import { useSetAtom } from 'jotai'
 import { Button } from '@/components/ui/button'
+import { memorizedRoutePathAtom } from '@/store'
 import { setEnabledExperimentalRouter } from '@/utils/experimental'
 import { useNavigate } from '@tanstack/react-router'
 import { SettingsCard, SettingsCardContent } from '../../_modules/settings-card'
 
 export default function ExperimentalSwitch() {
   const navigate = useNavigate()
+
+  const setMemorizedNavigate = useSetAtom(memorizedRoutePathAtom)
+
   const handleClick = () => {
     setEnabledExperimentalRouter(false)
-    navigate({ to: '/' })
+    navigate({ to: '/dashboard' })
+    setMemorizedNavigate('/dashboard')
   }
 
   return (

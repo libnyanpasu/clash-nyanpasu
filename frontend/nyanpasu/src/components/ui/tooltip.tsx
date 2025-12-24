301225003a8571fc6cd4dba1df8ede61b48f9db0
keiko233 <i@elaina.moe>
2025-12-25 00:23:40 +0800

    feat(tooltip): add Tooltip component

---
+++ frontend/nyanpasu/src/components/ui/tooltip.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import { cn } from '@nyanpasu/ui'
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+
+export function TooltipProvider({
+  delayDuration = 0,
+  ...props
+}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
+  return (
+    <TooltipPrimitive.Provider
+      data-slot="tooltip-provider"
+      delayDuration={delayDuration}
+      {...props}
+    />
+  )
+}
+
+export function Tooltip({
+  ...props
+}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
+  return (
+    <TooltipProvider>
+      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
+    </TooltipProvider>
+  )
+}
+
+export function TooltipTrigger({
+  ...props
+}: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
+  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
+}
+
+export function TooltipContent({
+  className,
+  sideOffset = 0,
+  children,
+  disableArrow = false,
+  ...props
+}: React.ComponentProps<typeof TooltipPrimitive.Content> & {
+  disableArrow?: boolean
+}) {
+  return (
+    <TooltipPrimitive.Portal>
+      <TooltipPrimitive.Content
+        data-slot="tooltip-content"
+        sideOffset={sideOffset}
+        className={cn(
+          'bg-surface text-on-surface',
+          'z-50 w-fit',
+          'rounded-full px-3 py-1.5 text-xs text-balance',
+          'shadow-outline/30 dark:shadow-surface-variant/20 shadow-sm',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+
+        {!disableArrow && (
+          <TooltipPrimitive.Arrow
+            className={cn(
+              'fill-surface z-50',
+              'h-2.5 w-4 translate-y-[-6px] rounded-xl',
+            )}
+          />
+        )}
+      </TooltipPrimitive.Content>
+    </TooltipPrimitive.Portal>
+  )
+}

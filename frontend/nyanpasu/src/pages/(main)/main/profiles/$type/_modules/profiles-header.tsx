612ef58e3c3265dc5ed029d050c529a11d7fedc6
keiko233 <i@elaina.moe>
2026-02-02 22:56:12 +0800

    chore(profiles): update ProfilesHeader styling and update shadow effect

--- frontend/nyanpasu/src/pages/(main)/main/profiles/$type/_modules/profiles-header.tsx
+++ frontend/nyanpasu/src/pages/(main)/main/profiles/$type/_modules/profiles-header.tsx
@@ -1,6 +1,7 @@
 import ArrowBackIosNewRounded from '~icons/material-symbols/arrow-back-ios-new-rounded'
 import { Button } from '@/components/ui/button'
 import useIsMobile from '@/hooks/use-is-moblie'
+import { cn } from '@nyanpasu/ui'
 import { Link } from '@tanstack/react-router'
 import ProfileQuickImport from '../../_modules/profile-quick-import'
 
@@ -18,7 +19,12 @@ export default function ProfilesHeader() {
   const isMobile = useIsMobile()
 
   return (
-    <div className="flex items-center justify-between gap-2 p-4 pb-0">
+    <div
+      className={cn(
+        'flex items-center justify-between gap-2 p-4 pb-0',
+        'sticky top-0 z-10',
+      )}
+    >
       {isMobile && <BackButton />}
 
       <ProfileQuickImport />

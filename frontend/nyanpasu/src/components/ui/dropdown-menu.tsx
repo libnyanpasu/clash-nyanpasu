39a7c1b0b39829ea5adba67d1466a28e50ac632d
keiko233 <i@elaina.moe>
2026-02-06 23:32:03 +0800

    feat(dropdown-menu): impl dropdown menu subcomponents

--- frontend/nyanpasu/src/components/ui/dropdown-menu.tsx
+++ frontend/nyanpasu/src/components/ui/dropdown-menu.tsx
@@ -1,3 +1,4 @@
+import ArrowRight from '~icons/material-symbols/arrow-right-rounded'
 import Check from '~icons/material-symbols/check-rounded'
 import RadioChecked from '~icons/material-symbols/radio-button-checked'
 import Radio from '~icons/material-symbols/radio-button-unchecked'
@@ -6,6 +7,50 @@ import { ComponentProps, createContext, useContext } from 'react'
 import { cn } from '@nyanpasu/ui'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { useControllableState } from '@radix-ui/react-use-controllable-state'
+
+const MotionContent = ({
+  children,
+  className,
+  ...props
+}: ComponentProps<typeof motion.div>) => {
+  return (
+    <motion.div
+      className={cn(
+        'relative z-50 w-full overflow-auto rounded',
+        'dark:text-on-surface',
+        'bg-inverse-on-surface dark:bg-surface',
+        'shadow shadow-zinc-300 dark:shadow-zinc-900',
+        className,
+      )}
+      style={{
+        maxHeight: 'var(--radix-popper-available-height)',
+      }}
+      initial={{
+        opacity: 0,
+        scaleY: 0.9,
+        transformOrigin: 'top',
+      }}
+      animate={{
+        opacity: 1,
+        scaleY: 1,
+        transformOrigin: 'top',
+      }}
+      exit={{
+        opacity: 0,
+        scaleY: 0.9,
+        transformOrigin: 'top',
+      }}
+      transition={{
+        type: 'spring',
+        bounce: 0,
+        duration: 0.35,
+      }}
+      {...props}
+    >
+      {children}
+    </motion.div>
+  )
+}
 
 const DropdownMenuContext = createContext<{
   open: boolean
@@ -52,7 +97,92 @@ export const DropdownMenuGroup = DropdownMenuPrimitive.Group
 
 export const DropdownMenuPortal = DropdownMenuPrimitive.Portal
 
-export const DropdownMenuSub = DropdownMenuPrimitive.Sub
+const DropdownMenuSubContext = createContext<{
+  open: boolean
+} | null>(null)
+
+const useDropdownMenuSubContext = () => {
+  const context = useContext(DropdownMenuSubContext)
+
+  if (context === null) {
+    throw new Error(
+      'DropdownMenuSub compound components cannot be rendered outside the DropdownMenuSub component',
+    )
+  }
+
+  return context
+}
+
+export const DropdownMenuSub = ({
+  open: inputOpen,
+  defaultOpen,
+  onOpenChange,
+  children,
+  ...props
+}: ComponentProps<typeof DropdownMenuPrimitive.Sub>) => {
+  const [open, setOpen] = useControllableState({
+    prop: inputOpen,
+    defaultProp: defaultOpen ?? false,
+    onChange: onOpenChange,
+  })
+
+  return (
+    <DropdownMenuSubContext.Provider
+      value={{
+        open,
+      }}
+    >
+      <DropdownMenuPrimitive.Sub {...props} open={open} onOpenChange={setOpen}>
+        {children}
+      </DropdownMenuPrimitive.Sub>
+    </DropdownMenuSubContext.Provider>
+  )
+}
+
+export function DropdownMenuSubTrigger({
+  children,
+  className,
+  ...props
+}: ComponentProps<typeof DropdownMenuPrimitive.SubTrigger>) {
+  return (
+    <DropdownMenuPrimitive.SubTrigger
+      className={cn(
+        'flex h-12 cursor-default items-center justify-between gap-2 p-4 pr-2 outline-hidden',
+        'cursor-pointer',
+        'hover:bg-surface-variant',
+        'dark:hover:bg-surface-variant',
+        'data-[state=open]:bg-surface-variant/30',
+        'dark:data-[state=open]:bg-surface-variant/30',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+
+      <ArrowRight className="text-outline-variant dark:text-outline size-6" />
+    </DropdownMenuPrimitive.SubTrigger>
+  )
+}
+
+export function DropdownMenuSubContent({
+  children,
+  className,
+  ...props
+}: ComponentProps<typeof DropdownMenuPrimitive.SubContent>) {
+  const { open } = useDropdownMenuSubContext()
+
+  return (
+    <AnimatePresence initial={false}>
+      {open && (
+        <DropdownMenuPortal forceMount>
+          <DropdownMenuPrimitive.SubContent {...props} asChild>
+            <MotionContent className={className}>{children}</MotionContent>
+          </DropdownMenuPrimitive.SubContent>
+        </DropdownMenuPortal>
+      )}
+    </AnimatePresence>
+  )
+}
 
 const DropdownMenuRadioGroupContext = createContext<{
   value: string | null
@@ -93,10 +223,6 @@ export const DropdownMenuRadioGroup = ({
   )
 }
 
-export const DropdownMenuSubTrigger = DropdownMenuPrimitive.SubTrigger
-
-export const DropdownMenuSubContent = DropdownMenuPrimitive.SubContent
-
 export const DropdownMenuContent = ({
   children,
   className,
@@ -109,39 +235,7 @@ export const DropdownMenuContent = ({
       {open && (
         <DropdownMenuPrimitive.Portal forceMount>
           <DropdownMenuPrimitive.Content {...props} asChild>
-            <motion.div
-              className={cn(
-                'shadow-container relative z-50 w-full overflow-auto rounded',
-                'dark:text-on-surface',
-                'bg-inverse-on-surface dark:bg-surface',
-                className,
-              )}
-              style={{
-                maxHeight: 'var(--radix-popper-available-height)',
-              }}
-              initial={{
-                opacity: 0,
-                scaleY: 0.9,
-                transformOrigin: 'top',
-              }}
-              animate={{
-                opacity: 1,
-                scaleY: 1,
-                transformOrigin: 'top',
-              }}
-              exit={{
-                opacity: 0,
-                scaleY: 0.9,
-                transformOrigin: 'top',
-              }}
-              transition={{
-                type: 'spring',
-                bounce: 0,
-                duration: 0.35,
-              }}
-            >
-              {children}
-            </motion.div>
+            <MotionContent className={className}>{children}</MotionContent>
           </DropdownMenuPrimitive.Content>
         </DropdownMenuPrimitive.Portal>
       )}

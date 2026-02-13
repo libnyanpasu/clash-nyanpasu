2e94a97626d687adcc6fce78bb4989c0bcabac4f
keiko233 <i@elaina.moe>
2026-02-13 18:00:20 +0800

    feat(settings): add external controller and port strategy configuration options

--- frontend/nyanpasu/src/pages/(main)/main/settings/web-ui/route.tsx
+++ frontend/nyanpasu/src/pages/(main)/main/settings/web-ui/route.tsx
@@ -1,9 +1,28 @@
+import { m } from '@/paraglide/messages'
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  SettingsTitle,
+  SettingsTitlePlaceholder,
+} from '../_modules/settings-title'
+import CoreSecretConfig from './_modules/core-secret-config'
+import ExternalControllerConfig from './_modules/external-controller-config'
+import PortStrategySelector from './_modules/port-strategy-selector'
 
 export const Route = createFileRoute('/(main)/main/settings/web-ui')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/(main)/main/settings/web-ui"!</div>
+  return (
+    <>
+      <SettingsTitlePlaceholder />
+      <SettingsTitle>{m.settings_label_external_controll()}</SettingsTitle>
+
+      <ExternalControllerConfig />
+
+      <PortStrategySelector />
+
+      <CoreSecretConfig />
+    </>
+  )
 }

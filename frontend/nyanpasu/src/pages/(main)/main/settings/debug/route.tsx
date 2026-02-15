2b88af1d3f974e7d6980da5654b3cd6e34a76079
keiko233 <i@elaina.moe>
2026-02-15 17:53:22 +0800

    feat(debug): add Block Task Viewer component to settings debug page

--- frontend/nyanpasu/src/pages/(main)/main/settings/debug/route.tsx
+++ frontend/nyanpasu/src/pages/(main)/main/settings/debug/route.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import BlockTaskViewer from './_modules/block-task-viewer'
 import WindowDebug from './_modules/window-debug'
 
 export const Route = createFileRoute('/(main)/main/settings/debug')({
@@ -9,6 +10,8 @@ function RouteComponent() {
   return (
     <div className="flex flex-col gap-4 p-4">
       <WindowDebug />
+
+      <BlockTaskViewer />
     </div>
   )
 }

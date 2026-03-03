c7dee32885498e6a514235804dc246bfebad4fb6
keiko233 <i@elaina.moe>
2026-03-03 14:48:35 +0800

    feat(settings): draft impl nyanpasu settings

--- frontend/nyanpasu/src/pages/(main)/main/settings/nyanpasu/route.tsx
+++ frontend/nyanpasu/src/pages/(main)/main/settings/nyanpasu/route.tsx
@@ -1,22 +1,31 @@
 import { m } from '@/paraglide/messages'
 import { createFileRoute } from '@tanstack/react-router'
-import {
-  SettingsTitle,
-  SettingsTitlePlaceholder,
-} from '../_modules/settings-title'
+import { SettingsLabel } from '../_modules/settings-card'
+import { SettingsTitle } from '../_modules/settings-title'
 import LogFileConfig from './_modules/log-file-config'
 
 export const Route = createFileRoute('/(main)/main/settings/nyanpasu')({
   component: RouteComponent,
 })
 
+const AppSettings = () => {
+  return (
+    <div data-slot="app-settings-container">
+      <SettingsLabel>{m.settings_label_nyanpasu()}</SettingsLabel>
+
+      <LogFileConfig />
+    </div>
+  )
+}
+
 function RouteComponent() {
   return (
     <>
-      <SettingsTitlePlaceholder />
       <SettingsTitle>{m.settings_label_nyanpasu()}</SettingsTitle>
 
-      <LogFileConfig />
+      <div className="space-y-4 px-4 pb-4">
+        <AppSettings />
+      </div>
     </>
   )
 }
